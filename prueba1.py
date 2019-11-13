2b8d5e46c19a2da82dad1949851d2c2fa679196c
tomas <tpampin95@gmail.com>
2019-11-13 09:58:41 -0300

    prueba1 handling error

--- prueba1.py
+++ prueba1.py
@@ -1,6 +1,12 @@
 num = (input("Ingresa 3 numeros separados por un espacio: ").split())
-numbers = [int(x) for x in num]
+no_numbers = True
 
+while no_numbers:
+    try:
+        numbers = [int(x) for x in num]
+        no_numbers = False
+    except:
+        num = (input("Ingresa 3 numeros separados por un espacio: ").split())
 
 while len(numbers) != 3:
     num = (input("Ingresa 3 numeros separados por un espacio: ").split())
